ba513c10c565dc09c74c48b27d36ed0c2ee431ab
drakosa1 <70101591@continental.edu.pe>
2026-06-06 22:15:39 -0500

    fix supabase url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // NaturaStock Cusco - Configuración de Supabase
 
 const SUPABASE_CONFIG = {
-  url: 'https://mwffklvdkalrcwuzixkg.supabase.co',
+  url: 'https://mwffklvdkalrcwuxizkg.supabase.co',
   anonKey: 'sb_publishable_oswhLhzhe6cAHNOmeIe3xQ_rz0bScru'
 };
